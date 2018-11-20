380b984b50258122907cc162bae2c45e542a8786
Matthew Tetreau <tetreaum@eos01.cis.gvsu.edu>
2018-11-20 17:59:57 -0500

    fixing pyaudio stuff

--- player/Player.py
+++ player/Player.py
@@ -37,27 +37,26 @@ class Player:
         self.wf = wave.open(track, 'rb')
 
         # instantiate PyAudio (1)
-        # self.p = pyaudio.PyAudio()
+        self.p = pyaudio.PyAudio()
 
         # open self.stream using callback (3)
-        # self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
-               # channels=self.wf.getnchannels(),
-               # rate=self.wf.getframerate(),
-               # output=True,
-               # stream_callback=self.callback)
+        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
+               channels=self.wf.getnchannels(),
+               rate=self.wf.getframerate(),
+               output=True,
+               stream_callback=self.callback)
 
         # start the self.stream (4)
-        # self.stream.start_stream()
+        self.stream.start_stream()
 
     def stop(self):
-       print ("I love python")
-       # self.stream.stop_stream()
-       # self.stream.close()
-       # self.wf.close()
+       self.stream.stop_stream()
+       self.stream.close()
+       self.wf.close()
 
-       # self.p.terminate() 
+       self.p.terminate() 
 
-   # def callback(self, in_data, frame_count, time_info, status):
-       # data = self.wf.readframes(frame_count)
-       # return (data, pyaudio.paContinue)
+    def callback(self, in_data, frame_count, time_info, status):
+       data = self.wf.readframes(frame_count)
+       return (data, pyaudio.paContinue)
 
